c5461e0407dd9d4922dfd40492e986dba38ab4c2
DR <deredious@gmail.com>
2025-04-26 12:07:07 +0300

    adding changes to List_noA.js

--- List_noA.js
+++ List_noA.js
@@ -1,2 +1,19 @@
-test
-
+var dictionary = [
+  "Wonderful",
+  "Joyful",
+  "Happiness",
+  "Time",
+  "Task",
+  "12344",
+  "Apple",
+  "Mark",
+  "If",
+  "Also",
+  "Laptop",
+  "Paramounts",
+  "Reorganisation",
+  "Truthful",
+];
+const rex = /\b[b-zB-Z]{6,}/;
+var result = dictionary.filter((word) => rex.test(word));
+console.log(result);
